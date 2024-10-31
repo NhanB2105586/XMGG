6abeea16f54fc398ec2229604239ba9f9da76eec
Vo Phu Qui <145702431+phuqui0103@users.noreply.github.com>
2024-11-01 02:10:55 +0700

    Them file text cấu hình MySQL

--- app/views/user/sanpham.php
+++ app/views/user/sanpham.php
@@ -54,28 +54,28 @@ include_once __DIR__ . '/../../models/product.php';
             </div>
             <div class="special-list row g-0 ">
                 <?php foreach ($products as $product): ?>
-                    <div class="product-item col-md-6 col-lg-4 col-xl-3 p-2 mb-3">
-                        <div class="special-img position-relative overflow-hidden">
-                            <img src="/images/upload/<?php echo $product['image_url']; ?>" class="w-100"
-                                alt="<?php echo htmlspecialchars($product['product_name']); ?>">
-                        </div>
-                        <div class="text-start m-1">
-                            <p class="text-capitalize mt-3 mb-1"><?php echo htmlspecialchars($product['product_name']); ?>
-                            </p>
-                            <div class="d-flex">
-                                <span
-                                    class="fw-bold d-block "><?php echo number_format($product['price'], 0, ',', '.') . 'đ'; ?></span>
-                                <span
-                                    class="price-old"><?php echo number_format($product['old_price'], 0, ',', '.') . 'đ'; ?>
-                                </span>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-around">
-                            <a href="#" class="btn btn-product mt-3 p-2" style="width: 45%;">Thêm Vào Giỏ</a>
-                            <a href="/products/<?php echo $product['product_id']; ?>"
-                                class="btn btn-product mt-3 p-2 btn-detail-product" style="width: 45%;">Chi Tiết</a>
+                <div class="product-item col-md-6 col-lg-4 col-xl-3 p-2 mb-3">
+                    <div class="special-img position-relative overflow-hidden">
+                        <img src="/images/upload/<?php echo $product['image_url']; ?>" class="w-100"
+                            alt="<?php echo htmlspecialchars($product['product_name']); ?>">
+                    </div>
+                    <div class="text-start m-1">
+                        <p class="text-capitalize mt-3 mb-1"><?php echo htmlspecialchars($product['product_name']); ?>
+                        </p>
+                        <div class="d-flex">
+                            <span
+                                class="fw-bold d-block "><?php echo number_format($product['price'], 0, ',', '.') . 'đ'; ?></span>
+                            <span
+                                class="price-old"><?php echo number_format($product['old_price'], 0, ',', '.') . 'đ'; ?>
+                            </span>
                         </div>
                     </div>
+                    <div class="d-flex justify-content-around">
+                        <a href="#" class="btn btn-product mt-3 p-2" style="width: 45%;">Thêm Vào Giỏ</a>
+                        <a href="/products/<?php echo $product['product_id']; ?>"
+                            class="btn btn-product mt-3 p-2 btn-detail-product" style="width: 45%;">Chi Tiết</a>
+                    </div>
+                </div>
                 <?php endforeach; ?>
             </div>
             <div class="text-center">
@@ -91,4 +91,5 @@ include_once __DIR__ . '/../../models/product.php';
 <?php
 include_once __DIR__ . '/../partials/app.php';
 include_once __DIR__ . '/../partials/foot.php';
+include_once __DIR__ . '/../partials/footer.php';
 ?>
