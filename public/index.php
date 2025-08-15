5df140e3be1216f30ac58ecf361fbad9d67dd861
TPHKhang <tphkhang210509@student.ctuet.edu.vn>
2025-08-15 16:47:50 +0700

    Fix merge conflict in public/index.php and add .env file

--- public/index.php
+++ public/index.php
@@ -26,31 +26,12 @@ define('APPNAME', 'Trang web nội thất Qui Nhan');
 
 $router = new \Bramus\Router\Router();
 
-<<<<<<< HEAD
 // Một cách gọn gàng hơn để tải tất cả các tệp route
 // Lệnh này sẽ tự động tải bất kỳ tệp .php nào từ thư mục routes.
 $routeFiles = glob(__DIR__ . '/../app/routes/*.php');
 foreach ($routeFiles as $routeFile) {
     require_once $routeFile;
 }
-=======
-session_start();
-require_once __DIR__ . '/../app/routes/admin.php';
-require_once __DIR__ . '/../app/routes/manageCustomer.php';
-require_once __DIR__ . '/../app/routes/manageCategorie.php';
-require_once __DIR__ . '/../app/routes/manageProduct.php';
-require_once __DIR__ . '/../app/routes/manageOrder.php';
-require_once __DIR__ . '/../app/routes/product.php';
-require_once __DIR__ . '/../app/routes/home.php';
-require_once __DIR__ . '/../app/routes/cart.php';
-require_once __DIR__ . '/../app/routes/user.php';
-require_once __DIR__ . '/../app/routes/orders.php';
-require_once __DIR__ . '/../app/routes/contact.php';
-require_once __DIR__ . '/../app/routes/manageContact.php';
-require_once __DIR__ . '/../app/routes/favorite.php';
-require_once __DIR__ . '/../app/routes/news.php';
-
->>>>>>> 7c425505595b6e785662ce5f53f9fbc09bd1405b
 
 $router->set404('\App\Controllers\Controller@sendNotFound');
 $router->run();
